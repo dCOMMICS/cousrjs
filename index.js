0ec744c01fd0d2903224d3e4c1c6b55ab6680cf2
dCOMMICS <sanwurld@gmail.com>
2023-10-05 00:35:16 +0300

    💪🏿🚀

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 console.log("%cHello, World", "color: blue; font-size: 40px"); // blue color
 
+var petDog = Rex;
+
+var petCat = Pepper;
+
+console.log(petDog);
+
+console.log(petCat);
+
+console.log(typeof petDog);
+
+console.log(typeof petCat);
+
+console.log("My pet dog's name is " + petDog);
+
+console.log("My pet cat's name is " + petCat);
+
