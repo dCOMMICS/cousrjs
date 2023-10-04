1ee9598f25f3d74c9491d0dcae72488c4018b61c
dCOMMICS <sanwurld@gmail.com>
2023-10-05 00:49:30 +0300

    ⚡

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-console.log("%cHello, World", "color: blue; font-size: 40px"); // blue color
+// console.log("%cHello, World", "color: blue; font-size: 40px"); // blue color
 
-var petDog = 'Rex';
+// var petDog = 'Rex';
 
-var petCat = 'Pepper';
+// var petCat = 'Pepper';
 
-console.log(petDog);
+// console.log(petDog);
 
-console.log(petCat);
+// console.log(petCat);
 
-console.log(typeof petDog);
+// console.log(typeof petDog);
 
-console.log(typeof petCat);
+// console.log(typeof petCat);
 
-console.log("My pet dog's name is " + petDog);
+// console.log("My pet dog's name is " + petDog);
 
-console.log("My pet cat's name is " + petCat);
+// console.log("My pet cat's name is " + petCat);
 
-var catSound = "purrr";
+// var catSound = "purrr";
 
-var dogSound = "woof";
+// var dogSound = "woof";
 
-// console.log(first)
+// // console.log(first)
 
 
-console.log(petDog, "says", dogSound);
+// console.log(petDog, "says", dogSound);
 
-console.log(petCat, "says", catSound);
+// console.log(petCat, "says", catSound);
 
-var petCat = 'Pepper';
-var catSound ="meow";
+// var petCat = 'Pepper';
+// var catSound ="meow";
 
-console.log(petCat, "says", catSound);
+// console.log(petCat, "says", catSound);
 
 
 var petDog1 = 'Jamila'
